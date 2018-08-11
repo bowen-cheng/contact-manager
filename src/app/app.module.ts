d8173a88e9c19461708a98b3e76ab351471f8d5e
Bowen Cheng <blueicycky@gmail.com>
2018-08-10 22:59:55 -0400

    Set up basic routing
    The main content component is now routed to.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+
+const routes: Routes = [
+  { path: '**', redirectTo: 'contacts' }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +19,8 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [ AppComponent ]
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MaterialModule } from '../shared/material.module';
 import { LayoutComponent } from './layout.component';
@@ -9,12 +10,17 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { SideNavComponent } from './sidenav/side-nav.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
+const route: Routes = [
+  { path: 'contacts', component: MainContentComponent }
+];
+
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forChild(route)
   ],
   declarations: [
     MainContentComponent,
