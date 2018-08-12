0973f7dfadad9c9f1c15d835ca0cc28c300d4dab
Bowen Cheng <blueicycky@gmail.com>
2018-08-12 08:28:02 -0400

    Fix several UI issues (with notes)

--- src/app/layout/main-content/main-content.component.ts
+++ src/app/layout/main-content/main-content.component.ts
@@ -1,25 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, NavigationStart, Params, Router, RouterEvent } from '@angular/router';
 
 import { User } from '../../model/user';
 import { UserService } from '../../services/user.service';
 
 @Component({
   templateUrl: './main-content.component.html',
-  styleUrls: [ './main-content.component.css' ]
+  styleUrls: ['./main-content.component.css']
 })
 export class MainContentComponent implements OnInit {
 
   protected user: User;
 
-  constructor(private route: ActivatedRoute, private userService: UserService) { }
+  constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      const id: number = +params[ 'id' ];
-      setTimeout(() => {
-        this.user = this.userService.getUserById(id);
-      }, 2000);
+      const id: number = +params['id'];
+      // Don't look up contact by id until the data service finished retrieving data
+      this.userService.users.subscribe(users => {
+        if (users.length === 0) {
+          return;
+        }
+        setTimeout(() => {
+          this.user = this.userService.getUserById(id);
+        }, 500);
+      });
+    });
+
+    // If a contact is already displayed, reset the content page upon router navigation (i.e. a new contact is selected)
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationStart) {
+        this.user = null;
+      }
     });
   }
 
--- src/app/layout/sidenav/side-nav.component.ts
+++ src/app/layout/sidenav/side-nav.component.ts
@@ -1,4 +1,6 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+import { NavigationStart, Router, RouterEvent } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { User } from '../../model/user';
@@ -9,27 +11,40 @@ const SMALL_BREAKPOINT = 720;
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: [ './side-nav.component.css' ]
+  styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent implements OnInit {
 
   // This mediaMatcher is a one-shot deal: it returns tells us if there is a match upon component creation
   private mediaMatcher: MediaQueryList = matchMedia(`(max-width: ${SMALL_BREAKPOINT}px)`);
 
-  users: Observable<User[]>;
+  protected users: Observable<User[]>;
 
-  constructor(private zone: NgZone, private userService: UserService) {
+  // Retrieves the MatSidenav type element with name "sideNav" element from the template
+  @ViewChild(MatSidenav) sideNav: MatSidenav;
+
+  constructor(private zone: NgZone, private userService: UserService, private router: Router) {
     // By adding this listener, we ask NgZone to detect screen size change
     this.mediaMatcher.addListener(mql => zone.run(() => this.mediaMatcher = mql));
-
-    this.users = this.userService.users;
-    this.userService.loadAll();
-    this.users.subscribe((data: User[]) => {
-      console.log(data);
-    });
   }
 
   ngOnInit() {
+    this.users = this.userService.users;
+    this.userService.loadAll();
+    this.users.subscribe((data: User[]) => {
+      // Navigate to the first contact page once the data service finished retrieving data
+      if (data.length > 0) {
+        console.log(data);
+        this.router.navigate(['contacts', data[0].id]);
+      }
+    });
+
+    // For small screens. close the side nav whenever a contact is selected (i.e. navigation starts)
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (this.isSmallScreen() && event instanceof NavigationStart) {
+        this.sideNav.close();
+      }
+    });
   }
 
   isSmallScreen(): boolean {
